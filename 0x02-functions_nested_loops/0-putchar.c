bc9f42dddb1ef92f167a034b2eb9b8c0a5146e8e
Abubakar Adamu <adamuabbakar@yahoo.com>
2023-03-21 07:07:18 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <main.h>
+/**
+ * main - Entry point 
+ * 
+ * Description: print _putchar followed by a new line 
+ *
+ * return: 0
+ */
+int main (void)
+{
+  _putchar("_putchar\n");
+  return (0);
+}
